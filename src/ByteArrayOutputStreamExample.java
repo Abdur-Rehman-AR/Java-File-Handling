27462cb3ead474d2ee5e2884d7da852c03f77314
Abdur Rehman <abdurrehman077.18@gmail.com>
2026-07-06 12:41:39 +0500

    feat: add code example transferring ByteArrayOutputStream data to a file.

--- src/ByteArrayOutputStreamExample.java
+++ src/ByteArrayOutputStreamExample.java
@@ -1,5 +1,4 @@
-import java.io.ByteArrayOutputStream;
-
+import java.io.*;
 public class ByteArrayOutputStreamExample {
 
     public static void main(String[] args) {
@@ -9,16 +8,29 @@ public class ByteArrayOutputStreamExample {
 
         /*
          * using an byte array as a buffer to store data, java first calculates the size
-         * of
-         * the bytes that got converted from data variable and then uses this size to
-         * give
-         * array the size.
+         * of the bytes that got converted from data variable and then uses this size to
+         * give array the size.
          */
         String data = "I am Abdur Rehman ...";
         byte[] arr = data.getBytes();
 
         // using write method to write all content from arr array to byte array in RAM
         outputStream.write(arr, 0, arr.length);
+
+        // Because this class cannot write to the file directly. So therefore we have 
+        // to use FileOutputStream to start a pipeline and then write to the file. 
+
+        System.out.println();
+        
+        try (FileOutputStream f = new FileOutputStream("File_1.txt");)
+        {
+            outputStream.writeTo(f);
+            System.out.println("Write to file successfully.");
+        } 
+        catch (IOException e) 
+        {
+            System.out.println("File not Found: " +e.getMessage());
+        }
 
         // getting the no. of bytes stored in array(internal buffer) in memory
 
@@ -32,6 +44,7 @@ public class ByteArrayOutputStreamExample {
         for (byte b : output) {
             System.out.print(((char) b));
         }
+        System.out.println();
 
         // Convert all bytes stored in the stream directly into a String.
 
